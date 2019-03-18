7208082a2f15aa38baff37e5cc8efcc6156e70f7
Linazzy <36626979+Linazzy@users.noreply.github.com>
2019-03-18 11:18:06 +0100

    principio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { TabsPage } from './../pages/tabs/tabs';
+import { LocalizationPage } from './../pages/localization/localization';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { InformacionPage } from '../pages/informacion/informacion';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Informacion', component: InformacionPage},
+      { title: 'Localizacion', component: TabsPage},
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
+import { ContactPage } from './../pages/contact/contact';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { TabsPage } from './../pages/tabs/tabs';
+import { CallNumber } from '@ionic-native/call-number';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { InformacionPage } from '../pages/informacion/informacion';
+import { LocalizationPage } from '../pages/localization/localization';
+import { EnlacesPage } from '../pages/enlaces/enlaces';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    InformacionPage,
+    LocalizationPage,
+    EnlacesPage,
+    ContactPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -23,11 +33,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    InformacionPage,
+    LocalizationPage,
+    EnlacesPage,
+    ContactPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    CallNumber,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
